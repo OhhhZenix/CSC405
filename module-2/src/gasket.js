21927cecd1cecee2c5560ecd2e277147bf8d75fd
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-08-25 00:20:33 -0600

    Update gasket.js

--- module-2/src/gasket.js
+++ module-2/src/gasket.js
@@ -1,11 +1,10 @@
 "use strict";
 
 const vertexShader = `
-attribute vec2 aPosition;
-uniform mat4 uTransform;
+attribute vec4 aPosition;
 
 void main() {
-	gl_Position = uTransform * vec4(aPosition, 0.0, 1.0);
+	gl_Position = aPosition;
 }
 `;
 
@@ -13,9 +12,14 @@ const fragmentShader = `
 precision mediump float;
 
 void main() {
-	gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); // White color
+	gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
 `;
+
+var canvas;
+var gl;
+var points = [];
+var numToSubDiv = 0;
 
 function compileShader(gl, source, type) {
   const shader = gl.createShader(type);
@@ -44,27 +48,77 @@ function createShaderProgram(gl, vertexSource, fragmentSource) {
   return program;
 }
 
+function addTriangle(a, b, c) {
+  points.push(a, b, c);
+}
+
+function divideTriangle(a, b, c, count) {
+  if (count == 0) {
+    addTriangle(a, b, c);
+  } else {
+    var ab = mix(a, b, 0.5);
+    var ac = mix(a, c, 0.5);
+    var bc = mix(b, c, 0.5);
+
+    count -= 1;
+
+    divideTriangle(a, ab, ac, count);
+    divideTriangle(c, ac, bc, count);
+    divideTriangle(b, bc, ab, count);
+  }
+}
+
+function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, points.length);
+  points = [];
+  requestAnimationFrame(main);
+}
+
 function main() {
-  const canvas = document.getElementById("gl-canvas");
+  canvas = document.getElementById("gl-canvas");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  const gl = canvas.getContext("webgl");
+  gl = canvas.getContext("webgl");
   if (!gl) {
     alert("Unable to init WebGL. Your browser or computer may not support it.");
     return;
   }
+
+  const vertices = [
+    [-1, -1],
+    [0, 1],
+    [1, -1],
+  ];
+  divideTriangle(vertices[0], vertices[1], vertices[2], numToSubDiv);
 
   const program = createShaderProgram(gl, vertexShader, fragmentShader);
   if (!program) {
     alert("Failed to compile shader!");
     return;
   }
-
   gl.useProgram(program);
 
+  const arrayBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, arrayBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, 50000, gl.STATIC_DRAW);
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
+
+  const aPosition = gl.getAttribLocation(program, "aPosition");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPosition);
+
+  document.getElementById("recursionDepth").onchange = function (event) {
+    numToSubDiv = parseInt(event.target.value);
+    document.getElementById("recursionDepthLabel").innerHTML =
+      "Recursion Depth: " + numToSubDiv;
+  };
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  render();
 }
 
-main();
+window.onload = main;
