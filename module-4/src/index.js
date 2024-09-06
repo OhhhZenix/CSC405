10215380e5eb40be6f1f390ec209ced3b3173b69
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-09-06 09:43:29 -0600

    Update index.js

--- module-4/src/index.js
+++ module-4/src/index.js
@@ -1,40 +1,41 @@
 "use strict";
 
-const vertexSource = `
+const vertexSource = `# version 300 es
 
+in vec4 aPosition;
+
+void main() {
+  gl_Position = aPosition;
+}
 `;
 
-const fragmentSource = `
+const fragmentSource = `# version 300 es
+precision highp float;
 
+in vec4 aColor;
+out vec4 fragColor;
+
+void main() {
+  fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
 `;
 
-// Renders the triangles onto the canvas using WebGL.
+// Renders onto the canvas using WebGL.
 function render(gl) {
   // Clear the canvas.
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   // Re-render on the next frame.
   requestAnimationFrame(() => render(gl));
 }
 
 function main() {
-  // Setup canvas
-  const canvas = document.getElementById("gl-canvas");
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
+  const canvas = createCanvas("gl-canvas");
+  const gl = initWebGL(canvas);
+  const program = createShaderProgram(gl, vertexSource, fragmentSource);
 
-  // Initialize WebGL context.
-  const gl = canvas.getContext("webgl");
-  if (!gl) {
-    alert("Unable to init WebGL. Your browser or computer may not support it.");
-    return;
-  }
-
-  // Set the viewport to match the canvas size.
-  gl.viewport(0, 0, canvas.width, canvas.height);
-  // Set the clear color (background color) to black.
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
+  // Run render
   render(gl);
 }
 
+// Starts the application
 window.onload = main;
